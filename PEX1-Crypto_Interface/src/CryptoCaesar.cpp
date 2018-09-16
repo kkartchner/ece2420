796e9eb7adca8ebf9db104feaea1de83661cb7b5
Ky Kartchner <ky.kartchner@aggiemail.usu.edu>
2018-09-15 19:53:35 -0600

    PEX1 final commit (hopefully)

--- PEX1-Crypto_Interface/src/CryptoCaesar.cpp
+++ PEX1-Crypto_Interface/src/CryptoCaesar.cpp
@@ -8,25 +8,37 @@ CryptoCaesar::CryptoCaesar(std::function<void(const uint8_t *data, uint32_t len)
  }
 
 void CryptoCaesar::genKeys()
-{	
-	uint8_t key[] = { 1 };
-	m_pubKey = key;
-	m_pubLen = (sizeof(key)/sizeof(*key));
+{
+	std::cout << "Generating CryptoCaesar keys..." << std::endl;
 	
-	std::cout << "m_pubLen is: " << m_pubLen << std::endl;
+	m_pubKey = new uint8_t(1);
+	m_pubLen = 1;
 	
+	m_priKey = nullptr;
+	m_priLen = 0;	
+	
+	std::cout << "Keys have been generated" << std::endl;
 }
 
 bool CryptoCaesar::getKeys(uint8_t **pubKey, uint32_t &pubLen,
                        uint8_t **priKey, uint32_t &priLen)
 {
+	std::cout << "Getting CryptoCaesar keys..." << std::endl;
+	
 	*pubKey = m_pubKey;
 	pubLen = m_pubLen;
+	
+	*priKey = m_priKey;
+	priLen = m_priLen;
+	
+	std::cout << "Keys have been received" << std::endl;
 }
 
 void CryptoCaesar::setKeys(const uint8_t *pubKey, uint32_t pubLen,
                        const uint8_t *priKey, uint32_t priLen)
 {	
+	std::cout << "Setting CryptoCaesar to specified keys..." << std::endl;
+	
 	if (m_pubKey != nullptr){
 	    *m_pubKey = *pubKey;
 	} else {
@@ -40,33 +52,55 @@ void CryptoCaesar::setKeys(const uint8_t *pubKey, uint32_t pubLen,
         m_priKey = new uint8_t(*priKey);
     }
 	m_priLen = priLen;
+	
+	std::cout << "Keys have been set" << std::endl;
 }
 
 void CryptoCaesar::destroyKeys()
 {
+	std::cout << "Destroying CryptoCaesar keys..." << std::endl;
+	delete(m_pubKey);
+	delete(m_priKey);
+	
+	m_pubKey = m_priKey = nullptr;	
+	m_pubLen = m_priLen = 0;
+	
+	std::cout << "Keys have been destroyed" << std::endl;
+}
+
+uint8_t shift_value (uint8_t value, uint8_t shift_amount){
+	uint8_t m_val = (value + shift_amount) % 256;
+	if (m_val < 0)
+		m_val = 256 - m_val;
+	return m_val;
 }
 
 bool CryptoCaesar::encrypt(const uint8_t *data, uint32_t len)
 {
-	uint8_t data_copy[len];
+	std::cout << "Encrypting data using CryptoCaesar..." << std::endl;
+	
+	uint8_t encrypted_data[len];
 	for (int i = 0; i < len; i++){
-		std::cout << "data[i] = " << data[i] << std::endl;
-		data_copy[i] = (data[i] - 1) % 256;
-		std::cout << "data_copy[i] = " << data_copy[i] << std::endl;
+		encrypted_data[i] = shift_value(data[i], -m_pubKey[0]);
 	}
 	
-	m_encryptCallback(data_copy, len);
+	m_encryptCallback(encrypted_data, len);
+	
+	std::cout << "Data has been encrypted" << std::endl;
 	return true;
 }
 
 bool CryptoCaesar::decrypt(const uint8_t *data, uint32_t len)
 {
-	uint8_t data_copy[len];
+	std::cout << "Decrypting data encrypted by CryptoCaesar..." << std::endl;
+	
+	uint8_t decrypted_data[len];
 	for (int i = 0; i < len; i++){
-		std::cout << "data[i] = " << data[i] << std::endl;
-		data_copy[i] = (data[i] + 1) % 256;
-		std::cout << "data_copy[i] = " << data_copy[i] << std::endl;
+		decrypted_data[i] = shift_value(data[i], m_pubKey[0]);
 	}
-	m_decryptCallback(data_copy, len);
+	
+	m_decryptCallback(decrypted_data, len);
+	
+	std::cout << "Data has been decrypted" << std::endl;
 	return true;
 } 
--- PEX1-Crypto_Interface/src/CryptoNone.cpp
+++ PEX1-Crypto_Interface/src/CryptoNone.cpp
@@ -9,23 +9,33 @@ CryptoNone::CryptoNone(std::function<void(const uint8_t *data, uint32_t len)> en
 
 void CryptoNone::genKeys()
 {
+	std::cout << "Generating CryptoNone keys..." << std::endl;
+	
 	m_pubKey = m_priKey = nullptr;
 	m_pubLen = m_priLen = 0;
+	
+	std::cout << "Keys have been generated" << std::endl;
 }
 
 bool CryptoNone::getKeys(uint8_t **pubKey, uint32_t &pubLen,
                        uint8_t **priKey, uint32_t &priLen)
 {
+	std::cout << "Getting CryptoNone keys..." << std::endl;
+	
 	*pubKey = m_pubKey;
 	pubLen = m_pubLen;	
 	
 	*priKey = m_priKey;
 	priLen = m_priLen;
+	
+	std::cout << "Keys have been received" << std::endl;
 }
 
 void CryptoNone::setKeys(const uint8_t *pubKey, uint32_t pubLen,
                        const uint8_t *priKey, uint32_t priLen)
 {
+	std::cout << "Setting CryptoNone to specified keys..." << std::endl;
+	
     if (m_pubKey != nullptr){
 	    *m_pubKey = *pubKey;
 	} else {
@@ -39,27 +49,40 @@ void CryptoNone::setKeys(const uint8_t *pubKey, uint32_t pubLen,
         m_priKey = new uint8_t(*priKey);
     }
 	m_priLen = priLen;
+	
+	std::cout << "Keys have been set" << std::endl;
 }
 
 void CryptoNone::destroyKeys()
 {
-	/*
+	std::cout << "Destroying CryptoNone keys..." << std::endl;
+	
 	delete(m_pubKey);
 	delete(m_priKey);
 	
 	m_pubKey = m_priKey = nullptr;	
 	m_pubLen = m_priLen = 0;
-	*/
+	
+	std::cout << "Keys have been destroyed" << std::endl;
 }
 
 bool CryptoNone::encrypt(const uint8_t *data, uint32_t len)
 {
+	std::cout << "\"Encrypting\" data using CryptoNone..." << std::endl;
+	
+	std::cout << "Data has been \"encrypted\". It is now impossible to read." << std::endl;
+	
 	m_encryptCallback(data, len);
+	
 	return true;
 }
 
 bool CryptoNone::decrypt(const uint8_t *data, uint32_t len)
 {
+	std::cout << "\"Decrypting\" data encrypted by CryptoNone..." << std::endl;
+	std::cout << "Data has been \"decrypted\". It is now possible to read." << std::endl;
+	
 	m_decryptCallback(data, len);
+	
 	return true;
 }
